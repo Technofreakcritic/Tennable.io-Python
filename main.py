d1e0d0850bc8324e6192e40e6cf6f176402c8d47
Arshaad Mohiadeen <arshaad919@gmail.com>
2023-10-18 04:01:21 +0000

    Added filter function to check  non license agents

--- main.py
+++ main.py
@@ -42,16 +42,16 @@ def get_list_of_dead_assets(response):
     # # Define the current time
     current_time = datetime.utcnow()
 
-    # # Calculate the threshold time (5 days ago)
-    threshold_time = current_time - timedelta(days=5)
+    # # Calculate the threshold time (2 days ago)
+    threshold_time = current_time - timedelta(days=7)
 
-    # # Initialize a count for instances more than 5 days old
+    # # Initialize a count for instances more than 2 days old
     count_old_instances = 0
 
     # Initialize a list to store the host IDs of instances more than 2 days old
     old_instance_ids = []
 
-    # # Iterate through instances to count those more than 5 days old
+    # # Iterate through instances to count those more than 2 days old
     for asset in json_data.get("assets", []):
         last_seen_value = asset.get("last_seen")
         if last_seen_value:
@@ -63,9 +63,8 @@ def get_list_of_dead_assets(response):
 
     # Print the count of instances more than 2 days old --- This works
     print(f"Count of instances more than 2 days old: {count_old_instances}")
-    # print(old_instance_ids)
+    print(old_instance_ids)
     return old_instance_ids
-
 
 
 # Let's goooooo MY API fucntion works
@@ -97,11 +96,45 @@ def delete_assets(headers, host_ids):
             print(f"Error deleting asset with host_id {host_id}: {e}")
 
 
+def get_host_details(headers, hosts):
+    current_time = datetime.utcnow()
+    threshold_time = current_time - timedelta(days=7)
+    
+    matching_hosts = []  # Initialize an empty list to store matching hosts
+    
+    for host in hosts:
+        url = f"https://cloud.tenable.com/assets/{host}"
+        response = requests.get(url, headers=headers)
+        
+        if response.status_code == 200:
+            host_data = response.json()
+            last_licensed_scan_date = host_data.get("last_licensed_scan_date")
+            last_seen = host_data.get("last_seen")
+
+            if last_licensed_scan_date is None and last_seen:
+                last_seen_time = datetime.strptime(last_seen, "%Y-%m-%dT%H:%M:%S.%fZ")
+                
+                if last_seen_time < threshold_time:
+                    matching_hosts.append(host_data.get("id"))  # Append the host data to the list
+                    # print(host_data)
+        else:
+            print(f"Failed to retrieve details for host: {host}")
+            print(f"Status code: {response.status_code}")
+    
+    return matching_hosts  # Return the list of matching hosts
+
 
 # Continue with the rest of the code, checking if response is not None before using it
 if response is not None:
     # Call the function with the response
     host_ids = get_list_of_dead_assets(response)
-    delete_assets(main_headers, host_ids)
+    license_check = get_host_details(main_headers, host_ids)
+    delete_assets(main_headers, license_check)
 else:
     print("API request failed. Cannot proceed.")
+
+
+
+
+
+
